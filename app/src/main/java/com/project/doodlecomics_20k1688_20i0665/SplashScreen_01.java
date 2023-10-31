3c1878260d2b4e6f9762201d352ba37f9a670fab
Aleena Fatima Khalid <ainookhalid@gmail.com>
2023-10-31 10:31:10 +0500

    Repo Creation, initial commit

---
+++ app/src/main/java/com/project/doodlecomics_20k1688_20i0665/SplashScreen_01.java
@@ -0,0 +1,14 @@
+package com.project.doodlecomics_20k1688_20i0665;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreen_01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen01);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "DoodleComics_20k1688_20i0665"
+include(":app")
+ 
