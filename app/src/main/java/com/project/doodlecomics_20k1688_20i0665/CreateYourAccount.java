b803434277f985fa13aecdb6668e228dcc8bb7dc
Aleena Fatima Khalid <ainookhalid@gmail.com>
2023-11-07 13:20:44 +0500

    create your account activity

---
+++ app/src/main/java/com/project/doodlecomics_20k1688_20i0665/CreateYourAccount.java
@@ -0,0 +1,14 @@
+package com.project.doodlecomics_20k1688_20i0665;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateYourAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_your_account);
+    }
+}
