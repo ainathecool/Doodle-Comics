caef9a73becad633e5b4ceaa54122eab75e81357
Aleena Fatima Khalid <ainookhalid@gmail.com>
2023-11-07 14:17:46 +0500

    login screens complete: reg module screens complete

---
+++ app/src/main/java/com/project/doodlecomics_20k1688_20i0665/ForgotPassword.java
@@ -0,0 +1,14 @@
+package com.project.doodlecomics_20k1688_20i0665;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+}
---
+++ app/src/main/java/com/project/doodlecomics_20k1688_20i0665/ResetPassword.java
@@ -0,0 +1,14 @@
+package com.project.doodlecomics_20k1688_20i0665;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
